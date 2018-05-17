b94c7ca456fc0cbea4e86aba1c7203079b208615
musicteachj <jimilittle3@hotmail.com>
2018-05-17 14:36:28 -0400

    routes
    testing routes and postman

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 console.log('Test');
 
 var express = require("express");
+var app = express();
 
+app.get("/", function(req, res) {
+	res.send("<h1 style='color:blue'>Hi there!</h1>");
+});
+
+app.get("/bye", function(req, res) {
+	res.send("<h1 style='color:red'>Goodbye!</h1>");
+});
+
+app.get("/dog", function(req, res) {
+	res.send("<h1 style='color:green'>Bark!</h1>");
+});
+
+app.listen(3000, function() {
+	console.log("Serving up on port 3000");
+});
