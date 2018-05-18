f790b0f083f4556bfdbc5d174625b9b1c17486ec
musicteachj <jimilittle3@hotmail.com>
2018-05-17 23:46:37 -0400

    update
    add comments and Route Params

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 console.log('Test');
 
+// Require Dependencies
 var express = require("express");
+var path = require("path");
 var app = express();
 
+//// Routes ////
+
+// Send HTML File
 app.get("/", function(req, res) {
-	res.send("<h1 style='color:blue'>Hi there!</h1>");
+	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("/bye", function(req, res) {
@@ -13,6 +18,19 @@ app.get("/bye", function(req, res) {
 
 app.get("/dog", function(req, res) {
 	res.send("<h1 style='color:green'>Bark!</h1>");
+});
+
+// Route Params Var
+app.get("/r/:subredditName", function(req, res) {
+	var subreddit = req.params.subredditName;
+	res.send("Welcome to the " + subreddit + " SubReddit!");
+})
+
+// Route for any other request not labeled above
+// Error page
+// Must be placed under other routes, order of routes matter
+app.get("*", function(req, res) {
+	res.send("<h1 style='color:purple'>Other Page!</h1>");
 });
 
 app.listen(3000, function() {
